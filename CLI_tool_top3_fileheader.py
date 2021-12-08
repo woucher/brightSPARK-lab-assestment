7619386c940e0d121f16d9183c5dc80e5b843222
Peter Maurice Cullen <u5798665@anu.edu.au>
2021-12-08 16:23:39 +1100

    first push using def and csv

---
+++ CLI_tool_top3_fileheader.py
@@ -0,0 +1,16 @@
+import csv #importing default python csv handler
+
+#Takes a file path are returns the csv file.
+def csv_import(local_file_path):
+    return None
+#Takes csv file and sorts by division and by score.
+def csv_sort(csv_file):
+    return None
+#Prints file header to commandline
+def print_file_head(csv_file, number_of_rows):
+    return None
+
+#This is called when file is called.
+if __name__ == '__main__':
+    file_path = None
+    print_file_head(csv_sort(csv_import(file_path)),3)
